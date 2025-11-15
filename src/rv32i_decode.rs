645ae8cf3e4fef21aca5688850e13b36546d10c5
Christopher Troyer <andytroyer2014@gmail.com>
2025-11-15 15:57:59 -0600

    Decode functions all laid out, still needs to fill in

--- src/rv32i_decode.rs
+++ src/rv32i_decode.rs
@@ -1,6 +1,6 @@
 use std::string;
 
-use crate::hex;
+use crate::hex::{self, to_hex0x20};
 
 const MNEMONIC_WIDTH:  u32 = 8;
 
@@ -78,7 +78,7 @@ fn get_func7(insn:u32) -> u32{
 
 //immediate values
 
-fn get_imm_i(insn:u32) -> u32{
+fn get_imm_i(insn:u32) -> i32{
     let mut imm_i:u32 = (insn & 0xfff00000) >> 20;
     if (imm_i & 0x800) != 0 {
         imm_i |= 0xfffff000;
@@ -86,30 +86,44 @@ fn get_imm_i(insn:u32) -> u32{
     imm_i
 }
 
-fn get_imm_u(insn:u32) -> u32{
+fn get_imm_u(insn:u32) -> i32{
     //Zero Extended on the right
-    let imm_u: u32 = insn & 0xfffff000;
+    let imm_u: i32 = insn as i32 & 0xfffff000;
     imm_u
 }
 
-fn get_imm_s(insn:u32) -> u32{
-    let mut imm_s: u32 = (insn & 0xfe000000) >> (25-5);
-    imm_s |= (insn & 0x00000f80) >> (7-0);
+fn get_imm_b(insn:u32) -> i32{
+    let mut imm_b: i32 = (insn as i32 & 0x80000000) >> (31-12);
+    imm_b |= (insn as i32  & 0x7e000000) >> (25-5);
+    imm_b |= (insn as i32  & 0x00000f00) >> (8-1);
+    imm_b |= (insn as i32  & 0x00000080) << (11-7);
+
+    //sign exted the left
+    if (insn as i32 & 0x80000000) != 0{
+        imm_b |= 0xffffe000;
+    }
+
+    imm_b
+}
+
+fn get_imm_s(insn:u32) -> i32{
+    let mut imm_s: i32 = (insn as i32  & 0xfe000000) >> (25-5);
+    imm_s |= (insn as i32  & 0x00000f80) >> (7-0);
     
-    if (insn & 0x80000000) != 0{
+    if (insn as i32  & 0x80000000) != 0{
         imm_s |= 0xfffff000; // sign-extend the left
     }
 
     imm_s
 }
 
-fn get_imm_j(insn:u32) -> u32{
-    let mut imm_j: u32 = (insn & 0x80000000) >> (31-20);
-    imm_j |= (insn & 0x7fe00000) >> (21-1);
-    imm_j |= (insn & 0x00100000) >> (20-11);
-    imm_j |= (insn & 0x000ff000);
+fn get_imm_j(insn:u32) -> i32{
+    let mut imm_j: i32 = (insn as i32  & 0x80000000) >> (31-20);
+    imm_j |= (insn as i32  & 0x7fe00000) >> (21-1);
+    imm_j |= (insn as i32  & 0x00100000) >> (20-11);
+    imm_j |= (insn as i32  & 0x000ff000);
 
-    if (insn & 0x80000000) != 0{
+    if (insn as i32 & 0x80000000) != 0{
         imm_j |= 0xffe00000;
     }
 
@@ -128,4 +142,82 @@ fn render_reg(r:u32) -> String{
 
 fn render_illegal_insn(insn:u32) -> String{
     "ERROR: UNIMPLEMENTED INSTRUCTION".to_string()
+}
+
+fn render_lui(insn:u32) -> String{
+    let rd = get_rd(insn);
+    let imm_u = get_imm_u(insn);
+    format!("{}{},{}",render_mnemonic("lui".to_string()),render_reg(rd),to_hex0x20((imm_u as u32>>12)&0x0fffff))
+}
+
+fn render_auipc(insn:u32) -> String{
+    let rd = get_rd(insn);
+    let imm_u = get_imm_u(insn);
+    format!("{}{},{}",render_mnemonic("auipc".to_string()),render_reg(rd),to_hex0x20((imm_u as u32>>12)&0x0fffff))
+}
+
+fn render_jal(addr:u32, insn:u32) -> String{
+    let rd = get_rd(insn);
+    let imm_j = get_imm_j(insn);
+    format!("{}{},{}",render_mnemonic("jal".to_string()),render_reg(rd),hex::to_hex0x32(imm_j as u32 + addr))
+}
+
+fn render_jalr(insn:u32) -> String{
+    let rd = get_rd(insn);
+    let rs1 = get_rs1(insn);
+    let imm_i = get_imm_i(insn);
+    format!("{}{},{}",render_mnemonic("jal".to_string()),render_reg(rd),render_base_disp(imm_i, rs1))
+}
+
+//types
+
+fn render_btype(addr:u32, insn:u32, mnemonic:String) -> String{
+    
+}
+
+fn render_itype_load(insn:u32, mnemonic:String) -> String{
+    
+}
+
+fn render_stype(insn:u32, mnemonic:String) -> String{
+    
+}
+
+fn render_itype_alu(insn:u32, mnemonic:String, imm_i:i32) -> String{
+    
+}
+
+fn render_rtype(insn:u32, mnemonic:String) -> String{
+    
+}
+
+//render controls
+
+fn render_ecall(insn:u32, mnemonic:String) -> String{
+    "ecall".to_string()
+}
+
+fn render_ebreak(insn:u32, mnemonic:String) -> String{
+    "ebreak".to_string()
+}
+
+//helpers
+
+fn render_csrrx(insn:u32, mnemonic:String) -> String{
+    
+}
+
+fn render_csrrxi(insn:u32, mnemonic:String) -> String{
+    
+}
+
+fn render_base_disp(base:i32, disp:u32) -> String{
+
+}
+
+//Big decode monster
+//Look into improving dispatch
+
+fn decode(addr:u32, insn:u32) -> String{
+
 }
