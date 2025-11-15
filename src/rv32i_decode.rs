e8c888af9db3f6b059299bd424ad4500e686b423
Christopher Troyer <andytroyer2014@gmail.com>
2025-11-14 22:30:21 -0600

    immediate values and begining of renders

--- src/rv32i_decode.rs
+++ src/rv32i_decode.rs
@@ -1,3 +1,5 @@
+use std::string;
+
 use crate::hex;
 
 const MNEMONIC_WIDTH:  u32 = 8;
@@ -75,3 +77,55 @@ fn get_func7(insn:u32) -> u32{
 }
 
 //immediate values
+
+fn get_imm_i(insn:u32) -> u32{
+    let mut imm_i:u32 = (insn & 0xfff00000) >> 20;
+    if (imm_i & 0x800) != 0 {
+        imm_i |= 0xfffff000;
+    }
+    imm_i
+}
+
+fn get_imm_u(insn:u32) -> u32{
+    //Zero Extended on the right
+    let imm_u: u32 = insn & 0xfffff000;
+    imm_u
+}
+
+fn get_imm_s(insn:u32) -> u32{
+    let mut imm_s: u32 = (insn & 0xfe000000) >> (25-5);
+    imm_s |= (insn & 0x00000f80) >> (7-0);
+    
+    if (insn & 0x80000000) != 0{
+        imm_s |= 0xfffff000; // sign-extend the left
+    }
+
+    imm_s
+}
+
+fn get_imm_j(insn:u32) -> u32{
+    let mut imm_j: u32 = (insn & 0x80000000) >> (31-20);
+    imm_j |= (insn & 0x7fe00000) >> (21-1);
+    imm_j |= (insn & 0x00100000) >> (20-11);
+    imm_j |= (insn & 0x000ff000);
+
+    if (insn & 0x80000000) != 0{
+        imm_j |= 0xffe00000;
+    }
+
+    imm_j
+}
+
+//render instructions
+
+fn render_mnemonic(m:String) -> String{
+    format!("{:<8}",m)
+}
+
+fn render_reg(r:u32) -> String{
+    format!("x{}",r)
+}
+
+fn render_illegal_insn(insn:u32) -> String{
+    "ERROR: UNIMPLEMENTED INSTRUCTION".to_string()
+}
